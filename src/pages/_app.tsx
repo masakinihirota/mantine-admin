9169aaad92657fa3cf4e22bb3d9e9616110642fd
masakinihirota <masakinihirota@gmail.com>
2023-02-11 19:55:46 +0900

    Mantineの初期設定完了

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import { type AppType } from "next/app";
-
 import { api } from "../utils/api";
-
 import "../styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />;
+      </MantineProvider>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
